74bfab10cabd1e754220263698d3c6413ee49cb3
Kevin Liao <kevin.liao@sothebys.com>
2024-08-05 17:52:07 -0400

    Initial check in

---
+++ interactive/20240804.js
@@ -0,0 +1,49 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "20240804_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 2,
+	        "restart": 5,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 63.552290406222966,
+	        "maxtilt": 20.9636992221262,
+	        "minpan": 0,
+	        "mintilt": -30.298184961106305
+	    },
+	"id": "20240804",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"20240804_tiles/20240804_t_1b_%0y_%0x.jpg"},"down":{"src":"20240804_tiles/20240804_t_1d_%0y_%0x.jpg"},"front":{"src":"20240804_tiles/20240804_t_1f_%0y_%0x.jpg"},"height":25458,"left":{"src":"20240804_tiles/20240804_t_1l_%0y_%0x.jpg"},"right":{"src":"20240804_tiles/20240804_t_1r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"20240804_tiles/20240804_t_1u_%0y_%0x.jpg"},"width":25458},{"back":{"src":"20240804_tiles/20240804_t_2b_%0y_%0x.jpg"},"down":{"src":"20240804_tiles/20240804_t_2d_%0y_%0x.jpg"},"front":{"src":"20240804_tiles/20240804_t_2f_%0y_%0x.jpg"},"height":12729,"left":{"src":"20240804_tiles/20240804_t_2l_%0y_%0x.jpg"},"right":{"src":"20240804_tiles/20240804_t_2r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"20240804_tiles/20240804_t_2u_%0y_%0x.jpg"},"width":12729},{"back":{"src":"20240804_tiles/20240804_t_3b_%0y_%0x.jpg"},"down":{"src":"20240804_tiles/20240804_t_3d_%0y_%0x.jpg"},"front":{"src":"20240804_tiles/20240804_t_3f_%0y_%0x.jpg"},"height":6365,"left":{"src":"20240804_tiles/20240804_t_3l_%0y_%0x.jpg"},"right":{"src":"20240804_tiles/20240804_t_3r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"20240804_tiles/20240804_t_3u_%0y_%0x.jpg"},"width":6365},{"back":{"src":"20240804_tiles/20240804_t_4b_%0y_%0x.jpg"},"down":{"src":"20240804_tiles/20240804_t_4d_%0y_%0x.jpg"},"front":{"src":"20240804_tiles/20240804_t_4f_%0y_%0x.jpg"},"height":3183,"left":{"src":"20240804_tiles/20240804_t_4l_%0y_%0x.jpg"},"right":{"src":"20240804_tiles/20240804_t_4r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"20240804_tiles/20240804_t_4u_%0y_%0x.jpg"},"width":3183},{"back":{"src":"20240804_tiles/20240804_t_5b_%0y_%0x.jpg"},"down":{"src":"20240804_tiles/20240804_t_5d_%0y_%0x.jpg"},"front":{"src":"20240804_tiles/20240804_t_5f_%0y_%0x.jpg"},"height":1592,"left":{"src":"20240804_tiles/20240804_t_5l_%0y_%0x.jpg"},"right":{"src":"20240804_tiles/20240804_t_5r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"20240804_tiles/20240804_t_5u_%0y_%0x.jpg"},"width":1592},{"back":{"src":"20240804_tiles/20240804_t_6b_%0y_%0x.jpg"},"down":{"src":"20240804_tiles/20240804_t_6d_%0y_%0x.jpg"},"front":{"src":"20240804_tiles/20240804_t_6f_%0y_%0x.jpg"},"height":796,"left":{"src":"20240804_tiles/20240804_t_6l_%0y_%0x.jpg"},"right":{"src":"20240804_tiles/20240804_t_6r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"20240804_tiles/20240804_t_6u_%0y_%0x.jpg"},"width":796},{"back":{"src":"20240804_tiles/20240804_t_7b_%0y_%0x.jpg"},"down":{"src":"20240804_tiles/20240804_t_7d_%0y_%0x.jpg"},"front":{"src":"20240804_tiles/20240804_t_7f_%0y_%0x.jpg"},"height":398,"left":{"src":"20240804_tiles/20240804_t_7l_%0y_%0x.jpg"},"right":{"src":"20240804_tiles/20240804_t_7r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"20240804_tiles/20240804_t_7u_%0y_%0x.jpg"},"width":398}],"multilevel":true,"preview":{"src":"20240804_tiles/20240804_preview.jpg"},"projection":"cubic"},
+	"textbox": [
+	        {
+	            "align": "top",
+	            "style": " color:#ffffff; font-family: Arial,Helvetica,sans-serif; text-shadow: 0px 0px 3.8px #000000, 0px 0px 2.5px #000000, 0px 0px 1.7px #000000; font-size: 12px; font-weight: bold; text-align:center;",
+	            "text": "Cygnus Region",
+	            "yoff": "4px"
+	        }
+	    ],
+	"view": {
+	        "hfov": 57.2,
+	        "pan": 31.78,
+	        "tilt": 0
+	    }
+},
+"version": "4.1"
+}
+}
