832d6593ca51a32541c45b11f6e5728771b8b2f2
Kevin Liao <kevin.liao@sothebys.com>
2024-10-08 08:02:07 -0400

    20241008 Cassiopeia update

--- interactive/cassiopeia/cassiopeia.js
+++ interactive/cassiopeia/cassiopeia.js
@@ -25,13 +25,13 @@ PanoramaStudioViewerParams = {
 	        "zoom": 1
 	    },
 	"camera": {
-	        "maxpan": 125.44747081712063,
-	        "maxtilt": 40.23346303501946,
+	        "maxpan": 117.30643127364439,
+	        "maxtilt": 35.26322272457158,
 	        "minpan": 0,
-	        "mintilt": -30.661478599221795
+	        "mintilt": -38.37107740153181
 	    },
 	"id": "cassiopeia",
-	"image":{"baseindex":1,"bitmap":[{"height":15745,"src":"cassiopeia_tiles/cassiopeia_t_1_%0y_%0x.jpg","tilesize":493,"width":27860},{"height":7873,"src":"cassiopeia_tiles/cassiopeia_t_2_%0y_%0x.jpg","tilesize":493,"width":13930},{"height":3937,"src":"cassiopeia_tiles/cassiopeia_t_3_%0y_%0x.jpg","tilesize":493,"width":6965},{"height":1969,"src":"cassiopeia_tiles/cassiopeia_t_4_%0y_%0x.jpg","tilesize":493,"width":3483},{"height":985,"src":"cassiopeia_tiles/cassiopeia_t_5_%0y_%0x.jpg","tilesize":493,"width":1742},{"height":493,"src":"cassiopeia_tiles/cassiopeia_t_6_%0y_%0x.jpg","tilesize":493,"width":871}],"multilevel":true,"preview":{"src":"cassiopeia_tiles/cassiopeia_preview.jpg"},"projection":"planar"},
+	"image":{"baseindex":1,"bitmap":[{"height":16353,"src":"cassiopeia_tiles/cassiopeia_t_1_%0y_%0x.jpg","tilesize":512,"width":26052},{"height":8177,"src":"cassiopeia_tiles/cassiopeia_t_2_%0y_%0x.jpg","tilesize":512,"width":13026},{"height":4089,"src":"cassiopeia_tiles/cassiopeia_t_3_%0y_%0x.jpg","tilesize":512,"width":6513},{"height":2045,"src":"cassiopeia_tiles/cassiopeia_t_4_%0y_%0x.jpg","tilesize":512,"width":3257},{"height":1023,"src":"cassiopeia_tiles/cassiopeia_t_5_%0y_%0x.jpg","tilesize":512,"width":1629},{"height":512,"src":"cassiopeia_tiles/cassiopeia_t_6_%0y_%0x.jpg","tilesize":512,"width":815}],"multilevel":true,"preview":{"src":"cassiopeia_tiles/cassiopeia_preview.jpg"},"projection":"planar"},
 	"view": {
 	        "mode": "fitInWindow"
 	    }
