8824a951391f50fbf52d09b549e4837ce8ea57aa
Kevin Liao <kevin.liao@sothebys.com>
2024-10-17 11:38:11 -0400

    20241017 changes for Cygnus

--- interactive/cygnus/cygnus.js
+++ interactive/cygnus/cygnus.js
@@ -25,13 +25,13 @@ PanoramaStudioViewerParams = {
 	        "zoom": 1
 	    },
 	"camera": {
-	        "maxpan": 130.16421780466726,
-	        "maxtilt": 41.88408858246633,
+	        "maxpan": 109.31719965427838,
+	        "maxtilt": 55.902256406935365,
 	        "minpan": 0,
-	        "mintilt": -59.96671805997326
+	        "mintilt": -55.408721253166426
 	    },
 	"id": "cygnus",
-	"image":{"baseindex":1,"bitmap":[{"height":22627,"src":"cygnus_tiles/cygnus_t_1_%0y_%0x.jpg","tilesize":480,"width":28917},{"height":11314,"src":"cygnus_tiles/cygnus_t_2_%0y_%0x.jpg","tilesize":480,"width":14459},{"height":5657,"src":"cygnus_tiles/cygnus_t_3_%0y_%0x.jpg","tilesize":480,"width":7230},{"height":2829,"src":"cygnus_tiles/cygnus_t_4_%0y_%0x.jpg","tilesize":480,"width":3615},{"height":1415,"src":"cygnus_tiles/cygnus_t_5_%0y_%0x.jpg","tilesize":480,"width":1808},{"height":708,"src":"cygnus_tiles/cygnus_t_6_%0y_%0x.jpg","tilesize":480,"width":904},{"height":354,"src":"cygnus_tiles/cygnus_t_7_%0y_%0x.jpg","tilesize":480,"width":452}],"multilevel":true,"preview":{"src":"cygnus_tiles/cygnus_preview.jpg"},"projection":"planar"},
+	"image":{"baseindex":1,"bitmap":[{"height":24729,"src":"cygnus_tiles/cygnus_t_1_%0y_%0x.jpg","tilesize":480,"width":24286},{"height":12365,"src":"cygnus_tiles/cygnus_t_2_%0y_%0x.jpg","tilesize":480,"width":12143},{"height":6183,"src":"cygnus_tiles/cygnus_t_3_%0y_%0x.jpg","tilesize":480,"width":6072},{"height":3092,"src":"cygnus_tiles/cygnus_t_4_%0y_%0x.jpg","tilesize":480,"width":3036},{"height":1546,"src":"cygnus_tiles/cygnus_t_5_%0y_%0x.jpg","tilesize":480,"width":1518},{"height":773,"src":"cygnus_tiles/cygnus_t_6_%0y_%0x.jpg","tilesize":480,"width":759},{"height":387,"src":"cygnus_tiles/cygnus_t_7_%0y_%0x.jpg","tilesize":480,"width":380}],"multilevel":true,"preview":{"src":"cygnus_tiles/cygnus_preview.jpg"},"projection":"planar"},
 	"view": {
 	        "mode": "fitInWindow"
 	    }
