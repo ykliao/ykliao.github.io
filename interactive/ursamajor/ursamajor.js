95d2de9092378eda6cc0483728e7fa6ba3f86361
Kevin Liao <kevin.liao@sothebys.com>
2024-12-24 02:54:06 -0500

    Add more

---
+++ interactive/ursamajor/ursamajor.js
@@ -0,0 +1,43 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "ursamajor_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 0.08,
+	        "reference": "screen",
+	        "restart": 5,
+	        "returnToLevel": true,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 44.92974683544307,
+	        "maxtilt": 36.23417721518989,
+	        "minpan": 0,
+	        "mintilt": -36.360759493670884
+	    },
+	"id": "ursamajor",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"ursamajor_tiles/ursamajor_t_1b_%y_%x.jpg"},"down":{"src":"ursamajor_tiles/ursamajor_t_1d_%y_%x.jpg"},"front":{"src":"ursamajor_tiles/ursamajor_t_1f_%y_%x.jpg"},"height":2606,"left":{"src":"ursamajor_tiles/ursamajor_t_1l_%y_%x.jpg"},"right":{"src":"ursamajor_tiles/ursamajor_t_1r_%y_%x.jpg"},"tilesize":480,"up":{"src":"ursamajor_tiles/ursamajor_t_1u_%y_%x.jpg"},"width":2606},{"back":{"src":"ursamajor_tiles/ursamajor_t_2b_%y_%x.jpg"},"down":{"src":"ursamajor_tiles/ursamajor_t_2d_%y_%x.jpg"},"front":{"src":"ursamajor_tiles/ursamajor_t_2f_%y_%x.jpg"},"height":1303,"left":{"src":"ursamajor_tiles/ursamajor_t_2l_%y_%x.jpg"},"right":{"src":"ursamajor_tiles/ursamajor_t_2r_%y_%x.jpg"},"tilesize":480,"up":{"src":"ursamajor_tiles/ursamajor_t_2u_%y_%x.jpg"},"width":1303},{"back":{"src":"ursamajor_tiles/ursamajor_t_3b_%y_%x.jpg"},"down":{"src":"ursamajor_tiles/ursamajor_t_3d_%y_%x.jpg"},"front":{"src":"ursamajor_tiles/ursamajor_t_3f_%y_%x.jpg"},"height":652,"left":{"src":"ursamajor_tiles/ursamajor_t_3l_%y_%x.jpg"},"right":{"src":"ursamajor_tiles/ursamajor_t_3r_%y_%x.jpg"},"tilesize":480,"up":{"src":"ursamajor_tiles/ursamajor_t_3u_%y_%x.jpg"},"width":652},{"back":{"src":"ursamajor_tiles/ursamajor_t_4b_%y_%x.jpg"},"down":{"src":"ursamajor_tiles/ursamajor_t_4d_%y_%x.jpg"},"front":{"src":"ursamajor_tiles/ursamajor_t_4f_%y_%x.jpg"},"height":326,"left":{"src":"ursamajor_tiles/ursamajor_t_4l_%y_%x.jpg"},"right":{"src":"ursamajor_tiles/ursamajor_t_4r_%y_%x.jpg"},"tilesize":480,"up":{"src":"ursamajor_tiles/ursamajor_t_4u_%y_%x.jpg"},"width":326}],"multilevel":true,"preview":{"src":"ursamajor_tiles/ursamajor_preview.jpg"},"projection":"cubic"},
+	"view": {
+	        "hfov": 40.44,
+	        "pan": 22.46,
+	        "tilt": 0
+	    }
+},
+"version": "4.2"
+}
+}
