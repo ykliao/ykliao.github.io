b91d842e7a7afcc21284657068aab9b226a826f7
Kevin Liao <kevin.liao@sothebys.com>
2025-05-11 09:14:58 -0400

    20250511 updates

--- interactive/cygnus/cygnus_controls.js
+++ interactive/cygnus/cygnus_controls.js
@@ -2,6 +2,10 @@ PanoramaStudioViewerParams = {
 "panoramaStudioViewer": {
 "actions": [
         {
+            "func": function(){ var enabled = this.viewer.gyroscopeEnabled(); var gb = this.get('gyrobutton'); if (gb){ gb.skin = enabled?gb.enabledskin:gb.disabledskin; gb.updateSkins(); } },
+            "id": "updateGyroButton"
+        },
+        {
             "func": function(){ if (!this.viewer.fullEquirectangular()) return; this.viewer.panTo(this.viewer.getView().pan,0,100,1.0,0,'easeInOutQuad',true); this.tween({'time': 1.0, 'lp': 0.0, 'transition': 'easeInOutQuad', 'onInit' : function(a,dstParams){ a.lp=a.viewer.fisheyeDistortion(); }, 'onUpdate': function(a){ a.viewer.setFisheyeDistortion(a.lp);}}); },
             "id": "leaveLittlePlanet"
         },
@@ -131,6 +135,19 @@ PanoramaStudioViewerParams = {
                     "yoff": 0
                 },
                 {
+                    "align": "center",
+                    "disabledskin": "shadow(5,0,0,rgba(0,0,0,1));comp(0.66);copy(defaultSkin,64,128,64,64,0,0,28,28);",
+                    "enabledskin": "shadow(5,0,0,rgba(0,0,0,1));comp(1.0);copy(defaultSkin,64,128,64,64,0,0,28,28);",
+                    "id": "gyrobutton",
+                    "index": 18,
+                    "onclick": function(){ var enabled = this.viewer.enableGyroscope(!this.viewer.gyroscopeEnabled()); } ,
+                    "priority": 3,
+                    "skin": "shadow(5,0,0,rgba(0,0,0,1));comp(0.66);copy(defaultSkin,64,128,64,64,0,0,28,28);",
+                    "visible": false,
+                    "xoff": 0,
+                    "yoff": 0
+                },
+                {
                     "align": "right",
                     "id": "fullscreenButton",
                     "index": 16,
@@ -183,22 +200,30 @@ PanoramaStudioViewerParams = {
                 "caption": "-"
             },
             {
+                "id": "gyroItem",
+                "onclick": function(){  var gb = this.get('gyrobutton'); if (gb){ gb.onclick(); } else { this.viewer.enableGyroscope(!this.viewer.gyroscopeEnabled()); } },
+                "oninit": function(){  this.caption = this.viewer.tr('Gyroscope Control'); }
+            },
+            {
+                "caption": "-"
+            },
+            {
                 "onclick": function(){ window.open('http://www.tshsoft.com','_blank'); },
                 "oninit": function(){  this.caption = this.viewer.tr('About PanoramaStudio...'); }
             }
         ],
-        "onshow": function(){ var view1 = this.getItem('normalView'); if (view1){ view1.visible = (this.viewer.webglAvailable && this.viewer.fullEquirectangular()) ? true : false; } var view2 = this.getItem('littlePlanetView'); if (view2){ view2.visible = (this.viewer.webglAvailable && this.viewer.fullEquirectangular())?true:false; }  this.update(); },
+        "onshow": function(){ var view1 = this.getItem('normalView'); if (view1){ view1.visible = (this.viewer.webglAvailable && this.viewer.fullEquirectangular()) ? true : false; } var view2 = this.getItem('littlePlanetView'); if (view2){ view2.visible = (this.viewer.webglAvailable && this.viewer.fullEquirectangular())?true:false; } var gyro = this.getItem('gyroItem'); if (gyro){ gyro.visible = this.viewer.gyroAvailable?true:false; }  this.update(); },
         "style": "background-color: rgba(255,255,255,0.8); box-shadow: 4px 4px 4px rgba(0,0,0,0.5); border-radius: 3px;"
     },
 "events": {
         "id": "mainEvents",
-        "onexit": function(){ this.viewer.action('hideMessage'); },
+        "onexit": function(){ this.viewer.action('hideMessage');  this.viewer.gyroWasEnabled = this.viewer.gyroAvailable&&this.viewer.gyroscopeEnabled();},
         "ongyroscopeavailable": function(available){ this.viewer.gyroAvailable = available; var o = this.get('gyrobutton'); if(!!o){ o.setVisible(available&&this.viewer.panoType()==0); } } ,
         "ongyroscopetoggle": function(enabled){ this.viewer.action('updateGyroButton'); },
-        "oninit": function(){ var g = this.viewer.gallery(); this.viewer.hasGallery = ((!!g) && g.length>1);  },
+        "oninit": function(){ var g = this.viewer.gallery(); this.viewer.hasGallery = ((!!g) && g.length>1); this.viewer.checkForGyroscope(); },
         "onplay": function(){ var o = this.get('playbutton'); if (!!o){ o.sbackup = o.skin; o.shbackup = o.skinhover; o.sabackup = o.skinactive; o.skin = 'shadow(3,0,0,rgba(0,0,0,1));copy(defaultSkin,128,0,64,64,0,0,28,28)'; o.skinhover = 'shadow(3,0,0,rgba(0,0,0,1));copy(defaultSkin,128,0,64,64,0,0,28,28)'; o.skinactive = 'shadow(3,0,0,rgba(0,0,0,1));copy(defaultSkin,128,0,64,64,0,0,28,28)'; o.updateSkins(); } } ,
         "onresize": function(){ this.viewer.action('resizeMap');  },
-        "onscenechanged": function(){},
+        "onscenechanged": function(){ var o = this.get('gyrobutton'); if(!!o){ o.setVisible(this.viewer.gyroAvailable&&this.viewer.panoType()==0); this.viewer.gyroAvailable&&this.viewer.panoType()==0&&this.viewer.gyroWasEnabled&&o.onclick(); } if (this.viewer.isVRModeEnabled()){this.viewer.enableGyroscope(!0);}},
         "onstartaudio": function(senderId){ if (senderId=='gAudio'||senderId=='lAudio'){var o = this.get('audiobutton'); if (!!o){ o.skin = o.playskin; o.skinhover = o.playskinhover; o.skinactive = o.playskinactive; o.updateSkins(); }} } ,
         "onstop": function(){ var o = this.get('playbutton'); if (!!o){ o.skin = o.sbackup; o.skinhover = o.shbackup; o.skinactive = o.sabackup; o.updateSkins(); } } ,
         "onstopaudio": function(senderId){ if (senderId=='gAudio'||senderId=='lAudio'){var o = this.get('audiobutton'); if (!!o){ o.skin = o.pauseskin; o.skinhover = o.pauseskinhover; o.skinactive = o.pauseskinactive; o.updateSkins(); }} } ,
@@ -211,6 +236,7 @@ PanoramaStudioViewerParams = {
         "de": {
             "About PanoramaStudio...": "&Uuml;ber PanoramaStudio...",
             "Fullscreen": "Vollbild",
+            "Gyroscope Control": "Gyroskop-Steuerung",
             "LittlePlanet View": "LittlePlanet-Ansicht",
             "Normal View": "Normale Ansicht"
         }
--- interactive/ursamajor/ursamajor.js
+++ interactive/ursamajor/ursamajor.js
@@ -25,15 +25,17 @@ PanoramaStudioViewerParams = {
 	        "zoom": 1
 	    },
 	"camera": {
-	        "maxpan": 17.1132238547969,
-	        "maxtilt": 7.720397579948144,
+	        "maxpan": 17.152117545376,
+	        "maxtilt": 19.73854796888506,
 	        "minpan": 0,
-	        "mintilt": -12.815471045808126
+	        "mintilt": -0.6417458945548791
 	    },
 	"id": "ursamajor",
-	"image":{"baseindex":1,"bitmap":[{"height":4562,"src":"ursamajor_tiles/ursamajor_t_1_%y_%x.jpg","tilesize":480,"width":3802},{"height":2281,"src":"ursamajor_tiles/ursamajor_t_2_%y_%x.jpg","tilesize":480,"width":1901},{"height":1141,"src":"ursamajor_tiles/ursamajor_t_3_%y_%x.jpg","tilesize":480,"width":951},{"height":571,"src":"ursamajor_tiles/ursamajor_t_4_%y_%x.jpg","tilesize":480,"width":476}],"multilevel":true,"preview":{"src":"ursamajor_tiles/ursamajor_preview.jpg"},"projection":"planar"},
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"ursamajor_tiles/ursamajor_t_1b_%0y_%0x.jpg"},"down":{"src":"ursamajor_tiles/ursamajor_t_1d_%0y_%0x.jpg"},"front":{"src":"ursamajor_tiles/ursamajor_t_1f_%0y_%0x.jpg"},"height":25454,"left":{"src":"ursamajor_tiles/ursamajor_t_1l_%0y_%0x.jpg"},"right":{"src":"ursamajor_tiles/ursamajor_t_1r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"ursamajor_tiles/ursamajor_t_1u_%0y_%0x.jpg"},"width":25454},{"back":{"src":"ursamajor_tiles/ursamajor_t_2b_%0y_%0x.jpg"},"down":{"src":"ursamajor_tiles/ursamajor_t_2d_%0y_%0x.jpg"},"front":{"src":"ursamajor_tiles/ursamajor_t_2f_%0y_%0x.jpg"},"height":12727,"left":{"src":"ursamajor_tiles/ursamajor_t_2l_%0y_%0x.jpg"},"right":{"src":"ursamajor_tiles/ursamajor_t_2r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"ursamajor_tiles/ursamajor_t_2u_%0y_%0x.jpg"},"width":12727},{"back":{"src":"ursamajor_tiles/ursamajor_t_3b_%0y_%0x.jpg"},"down":{"src":"ursamajor_tiles/ursamajor_t_3d_%0y_%0x.jpg"},"front":{"src":"ursamajor_tiles/ursamajor_t_3f_%0y_%0x.jpg"},"height":6364,"left":{"src":"ursamajor_tiles/ursamajor_t_3l_%0y_%0x.jpg"},"right":{"src":"ursamajor_tiles/ursamajor_t_3r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"ursamajor_tiles/ursamajor_t_3u_%0y_%0x.jpg"},"width":6364},{"back":{"src":"ursamajor_tiles/ursamajor_t_4b_%0y_%0x.jpg"},"down":{"src":"ursamajor_tiles/ursamajor_t_4d_%0y_%0x.jpg"},"front":{"src":"ursamajor_tiles/ursamajor_t_4f_%0y_%0x.jpg"},"height":3182,"left":{"src":"ursamajor_tiles/ursamajor_t_4l_%0y_%0x.jpg"},"right":{"src":"ursamajor_tiles/ursamajor_t_4r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"ursamajor_tiles/ursamajor_t_4u_%0y_%0x.jpg"},"width":3182},{"back":{"src":"ursamajor_tiles/ursamajor_t_5b_%0y_%0x.jpg"},"down":{"src":"ursamajor_tiles/ursamajor_t_5d_%0y_%0x.jpg"},"front":{"src":"ursamajor_tiles/ursamajor_t_5f_%0y_%0x.jpg"},"height":1591,"left":{"src":"ursamajor_tiles/ursamajor_t_5l_%0y_%0x.jpg"},"right":{"src":"ursamajor_tiles/ursamajor_t_5r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"ursamajor_tiles/ursamajor_t_5u_%0y_%0x.jpg"},"width":1591},{"back":{"src":"ursamajor_tiles/ursamajor_t_6b_%0y_%0x.jpg"},"down":{"src":"ursamajor_tiles/ursamajor_t_6d_%0y_%0x.jpg"},"front":{"src":"ursamajor_tiles/ursamajor_t_6f_%0y_%0x.jpg"},"height":796,"left":{"src":"ursamajor_tiles/ursamajor_t_6l_%0y_%0x.jpg"},"right":{"src":"ursamajor_tiles/ursamajor_t_6r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"ursamajor_tiles/ursamajor_t_6u_%0y_%0x.jpg"},"width":796},{"back":{"src":"ursamajor_tiles/ursamajor_t_7b_%0y_%0x.jpg"},"down":{"src":"ursamajor_tiles/ursamajor_t_7d_%0y_%0x.jpg"},"front":{"src":"ursamajor_tiles/ursamajor_t_7f_%0y_%0x.jpg"},"height":398,"left":{"src":"ursamajor_tiles/ursamajor_t_7l_%0y_%0x.jpg"},"right":{"src":"ursamajor_tiles/ursamajor_t_7r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"ursamajor_tiles/ursamajor_t_7u_%0y_%0x.jpg"},"width":398}],"multilevel":true,"preview":{"src":"ursamajor_tiles/ursamajor_preview.jpg"},"projection":"cubic"},
 	"view": {
-	        "mode": "fitInWindow"
+	        "hfov": 15.44,
+	        "pan": 8.58,
+	        "tilt": 0
 	    }
 },
 "version": "4.2"
